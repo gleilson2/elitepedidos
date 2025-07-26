a626727042fc0593ba6de33a3963d39ef39ca38a
gleilson2 <gleilson.1@yahoo.com>
2025-07-26 19:35:09 -0300

    Updated ProductsPanel.tsx

--- src/components/Admin/ProductsPanel.tsx
+++ src/components/Admin/ProductsPanel.tsx
@@ -370,6 +370,7 @@ const ProductsPanel: React.FC = () => {
         error,
         formData,
         editingProduct
+      });
     }
   };
 
@@ -838,15 +839,6 @@ const ProductsPanel: React.FC = () => {
                   </div>
 
                   {/* Coluna Direita - Tamanhos */}
-                    <button
-                      onClick={() => handleScheduleProduct(product)}
-                      className="text-orange-600 hover:text-orange-800"
-                      title="Programar disponibilidade"
-                    >
-                      <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                      </svg>
-                    </button>
                   <div className="space-y-4">
                     <div className="border rounded-lg p-4">
                       <h4 className="font-semibold mb-4">Tamanhos do Produto</h4>
